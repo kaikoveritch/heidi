2d854b50210c5816d62f6dffd562ef465c1f78a1
kaikoveritch <kaiko.goren@gmail.com>
2017-05-14 20:26:08 +0200

    Created the base folders and files. Created the basic syntax. Created semantics for the 1st and 2nd approach and tests to verify them. Added the proof for part 2. Everything seems to be working as of now.

---
+++ Sources/heidi.swift
@@ -0,0 +1,381 @@
+import LogicKit
+
+
+// This is a utility function, not linked directly to the syntax or the semantic
+func reverse_aux (_ L: Term, _ R: Term, _ A: Term) -> Goal { // A is the accumulator
+   return
+      // If L is empty, the reversal is over and R takes the value of the accumulator
+      (L === List.empty && R === A)
+      ||
+      // Otherwise, the function is called recursively with the first element of
+      // L extracted and added to the accumulator (R stays untouched)
+      (
+         fresh{el in fresh{H in
+            L === List.cons(el, H)
+            &&
+            delayed(reverse_aux (H, R, List.cons(el,A)))
+         }}
+      )
+}
+func reverse (_ L: Term, _ R: Term) -> Goal { // L is the original list, R the reversed one
+   return
+      // Call the auxiliary with an empty accumulator
+      reverse_aux (L, R, List.empty)
+}
+
+
+// W = {whistles}
+// O = {orders}
+// HE = {Heidi Expressions}
+// TE = {Tita Expressions}
+
+
+//==============================================================================
+// Heidi's syntax
+//==============================================================================
+
+// o in {deponer, dretg, sanester, davent, davos, plaun, returnar, sa fermar}
+//───────────────────────────────────────────────────────────────────────────
+// o in O
+let deponer    = Value("deponer")
+let dretg      = Value("dretg")
+let sanester   = Value("sanester")
+let davent     = Value("davent")
+let davos      = Value("davos")
+let plaun      = Value("plaun")
+let returnar   = Value("returnar")
+let sa_fermar  = Value("sa fermar")
+
+// o in O
+//───────────
+// o;[] in HE
+//
+// o in O, e in HE
+//────────────────
+// o;e in HE
+func toHeidi (_ seq: Array<Term>) -> Term {
+
+   var newSeq = List.empty;
+
+   for order in seq.reversed() {
+       newSeq = List.cons(order,newSeq)
+   }
+
+   return newSeq
+}
+
+
+//==============================================================================
+// Tita's syntax
+//==============================================================================
+
+// o in {short, long, whee, who, wheet, wheeo, hee, pause}
+//────────────────────────────────────────────────────────
+// o in W
+let short   = Value("short")
+let long    = Value("long")
+let whee    = Value("whee")
+let who     = Value("who")
+let wheet   = Value("wheet")
+let wheeo   = Value("wheeo")
+let hee     = Value("hee")
+let pause   = Value("pause")
+
+// o in W
+//───────────
+// o;[] in TE
+//
+// o in W, e in TE
+//────────────────
+// o;e in TE
+func toTita (_ seq: Array<Term>) -> Term {
+
+   var newSeq = List.empty;
+
+   for order in seq.reversed() {
+       newSeq = List.cons(order,newSeq)
+   }
+
+   return newSeq
+}
+
+
+//==============================================================================
+// Heidi -> Tita - 1st semantic
+//==============================================================================
+
+func heidi2tita1_aux (_ order: Term, _ translation: Term, _ acc: Term) -> Goal {
+   return
+      (order === List.empty && translation === acc)
+      ||
+      fresh{L in
+         (  // deponer
+            order === List.cons(deponer,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(short,List.cons(short,List.cons(pause,acc)))))
+         )
+         ||
+         (  // dretg
+            order === List.cons(dretg,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(whee,List.cons(who,List.cons(pause,acc)))))
+         )
+         ||
+         (  //sanester
+            order === List.cons(sanester,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(wheet,List.cons(wheeo,List.cons(pause,acc)))))
+         )
+         ||
+         (  //davent
+            order === List.cons(davent,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(wheet,List.cons(wheeo,List.cons(wheet,List.cons(wheet,List.cons(pause,acc)))))))
+         )
+         ||
+         (  //davos
+            order === List.cons(davos,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(who,List.cons(hee,List.cons(who,List.cons(pause,acc))))))
+         )
+         ||
+         (  //plaun
+            order === List.cons(plaun,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(hee,List.cons(hee,List.cons(hee,List.cons(hee,List.cons(pause,acc)))))))
+         )
+         ||
+         (  //returnar
+            order === List.cons(returnar,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(whee,List.cons(whee,List.cons(wheet,List.cons(pause,acc))))))
+         )
+         ||
+         (  //sa fermar
+            order === List.cons(sa_fermar,L) &&
+            delayed(heidi2tita1_aux(L,translation,List.cons(long,List.cons(pause,acc))))
+         )
+      }
+}
+
+func heidi2tita1 (_ order: Term, _ translation: Term) -> Goal {
+   // order -HE-> []
+   //─────────────────────
+   // translation -TE-> []
+   //
+   // order -HE-> L;deponer;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';short;short;pause;[]
+   //
+   // order -HE-> L;dretg;[], L -TE-> L'
+   //───────────────────────────────────────
+   // translation -TE-> L';whee;who;pause;[]
+   //
+   // order -HE-> L;sanester;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';wheet;wheeo;pause;[]
+   //
+   // order -HE-> L;davent;[], L -TE-> L'
+   //──────────────────────────────────────────────────────
+   // translation -TE-> L';wheet;wheeo;wheet;wheet;pause;[]
+   //
+   // order -HE-> L;davos;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';who;hee;who;pause;[]
+   //
+   // order -HE-> L;plaun;[], L -TE-> L'
+   //──────────────────────────────────────────────
+   // translation -TE-> L';hee;hee;hee;hee;pause;[]
+   //
+   // order -HE-> L;returnar;[], L -TE-> L'
+   //──────────────────────────────────────────────
+   // translation -TE-> L';whee;whee;wheet;pause;[]
+   //
+   // order -HE-> L;sa_fermar;[], L -TE-> L'
+   //───────────────────────────────────────
+   // translation -TE-> L';long;pause;[]
+   return
+      fresh{revorder in
+         reverse(order,revorder) && heidi2tita1_aux(revorder,translation,List.empty)
+      }
+}
+
+func tita2heidi1 (_ order: Term, _ translation: Term) -> Goal {
+   // order -TE-> o1;pause;O, o1;pause;[] -HE-> t;[], O -HE-> T
+   //──────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   //
+   // order -TE-> o1;o2;pause;O, o1;o2;pause[] -HE-> t;[], O -HE-> T
+   //───────────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   //
+   // order -TE-> o1;o2;o3;pause;O, o1;o2;o3;pause[] -HE-> t;[], O -HE-> T
+   //─────────────────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   //
+   // order -TE-> o1;o2;o3;o4;pause;O, o1;o2;o3;o4;pause[] -HE-> t;[], O -HE-> T
+   //───────────────────────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   return
+      (order === List.empty && translation === List.empty)
+      ||
+      freshn{v in
+         let t = v["t"]
+         let T = v["T"]
+         let o1 = v["o1"]
+         let o2 = v["o2"]
+         let o3 = v["o3"]
+         let o4 = v["o4"]
+         let O = v["O"]
+         return
+            translation === List.cons(t,T) &&
+            (
+               // 1 whistle
+               (order === List.cons(o1,List.cons(pause,O))
+               &&
+               heidi2tita1(List.cons(t,List.empty),List.cons(o1,List.cons(pause,List.empty))))
+               ||
+               // 2 whistles
+               (order === List.cons(o1,List.cons(o2,List.cons(pause,O)))
+               &&
+               heidi2tita1(List.cons(t,List.empty),List.cons(o1,List.cons(o2,List.cons(pause,List.empty)))))
+               ||
+               // 3 whistles
+               (order === List.cons(o1,List.cons(o2,List.cons(o3,List.cons(pause,O))))
+               &&
+               heidi2tita1(List.cons(t,List.empty),List.cons(o1,List.cons(o2,List.cons(o3,List.cons(pause,List.empty))))))
+               ||
+               // 4 whistles
+               (order === List.cons(o1,List.cons(o2,List.cons(o3,List.cons(o4,List.cons(pause,O)))))
+               &&
+               heidi2tita1(List.cons(t,List.empty),List.cons(o1,List.cons(o2,List.cons(o3,List.cons(o4,List.cons(pause,List.empty)))))))
+            )
+            &&
+            delayed(tita2heidi1(O,T))
+      }
+}
+
+
+//==============================================================================
+// Heidi -> Tita - 2nd semantic
+//==============================================================================
+
+func heidi2tita2_aux (_ order: Term, _ translation: Term, _ acc: Term) -> Goal {
+   return
+      (order === List.empty && translation === acc)
+      ||
+      fresh{L in
+         (  // deponer
+            order === List.cons(deponer,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheeo,List.cons(hee,List.cons(wheet,List.cons(pause,acc))))))
+         )
+         ||
+         (  // dretg
+            order === List.cons(dretg,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(hee,List.cons(wheet,List.cons(pause,acc)))))
+         )
+         ||
+         (  //sanester
+            order === List.cons(sanester,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheet,List.cons(wheeo,List.cons(pause,acc)))))
+         )
+         ||
+         (  //davent
+            order === List.cons(davent,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheet,List.cons(hee,List.cons(wheet,List.cons(pause,acc))))))
+         )
+         ||
+         (  //davos
+            order === List.cons(davos,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheet,List.cons(wheeo,List.cons(wheet,List.cons(pause,acc))))))
+         )
+         ||
+         (  //plaun
+            order === List.cons(plaun,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheet,List.cons(wheeo,List.cons(wheeo,List.cons(pause,acc))))))
+         )
+         ||
+         (  //returnar
+            order === List.cons(returnar,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheeo,List.cons(wheet,List.cons(pause,acc)))))
+         )
+         ||
+         (  //sa fermar
+            order === List.cons(sa_fermar,L) &&
+            delayed(heidi2tita2_aux(L,translation,List.cons(wheeo,List.cons(wheeo,List.cons(pause,acc)))))
+         )
+      }
+}
+
+func heidi2tita2 (_ order: Term, _ translation: Term) -> Goal {
+   // order -HE-> []
+   //─────────────────────
+   // translation -TE-> []
+   //
+   // order -HE-> L;deponer;[], L -TE-> L'
+   //──────────────────────────────────────────────
+   // translation -TE-> L';wheeo;hee;wheet;pause;[]
+   //
+   // order -HE-> L;dretg;[], L -TE-> L'
+   //────────────────────────────────────────
+   // translation -TE-> L';hee;wheet;pause;[]
+   //
+   // order -HE-> L;sanester;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';wheet;wheeo;pause;[]
+   //
+   // order -HE-> L;davent;[], L -TE-> L'
+   //──────────────────────────────────────────────
+   // translation -TE-> L';wheet;hee;wheet;pause;[]
+   //
+   // order -HE-> L;davos;[], L -TE-> L'
+   //────────────────────────────────────────────────
+   // translation -TE-> L';wheet;wheeo;wheet;pause;[]
+   //
+   // order -HE-> L;plaun;[], L -TE-> L'
+   //────────────────────────────────────────────────
+   // translation -TE-> L';wheet;wheeo;wheeo;pause;[]
+   //
+   // order -HE-> L;returnar;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';wheeo;wheet;pause;[]
+   //
+   // order -HE-> L;sa_fermar;[], L -TE-> L'
+   //──────────────────────────────────────────
+   // translation -TE-> L';wheeo;wheeo;pause;[]
+   return
+      fresh{revorder in
+         reverse(order,revorder) && heidi2tita2_aux(revorder,translation,List.empty)
+      }
+}
+
+func tita2heidi2 (_ order: Term, _ translation: Term) -> Goal {
+   // order -TE-> o1;o2;pause;O, o1;o2;pause[] -HE-> t;[], O -HE-> T
+   //───────────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   //
+   // order -TE-> o1;o2;o3;pause;O, o1;o2;o3;pause[] -HE-> t;[], O -HE-> T
+   //─────────────────────────────────────────────────────────────────────
+   // translation -HE-> t;T
+   return
+      (order === List.empty && translation === List.empty)
+      ||
+      freshn{v in
+         let t  = v["t"]
+         let T  = v["T"]
+         let o1 = v["o1"]
+         let o2 = v["o2"]
+         let o3 = v["o3"]
+         let O  = v["O"]
+         return
+            translation === List.cons(t,T) &&
+            (
+               // 2 whistles
+               (order === List.cons(o1,List.cons(o2,List.cons(pause,O)))
+               &&
+               heidi2tita2(List.cons(t,List.empty),List.cons(o1,List.cons(o2,List.cons(pause,List.empty)))))
+               ||
+               // 3 whistles
+               (order === List.cons(o1,List.cons(o2,List.cons(o3,List.cons(pause,O))))
+               &&
+               heidi2tita2(List.cons(t,List.empty),List.cons(o1,List.cons(o2,List.cons(o3,List.cons(pause,List.empty))))))
+            )
+            &&
+            delayed(tita2heidi2(O,T))
+      }
+}
+
+// PROOF: In the test file as the "proof()" test.
---
+++ Tests/heidiTests/heidiTests.swift
@@ -0,0 +1,297 @@
+import XCTest
+import LogicKit
+@testable import heidi
+
+// Wrapper struct and resultsOf function
+// Shamefully ripped off from the minotaurTests file in order to test the
+// results in a manageable manner.
+
+struct Wrapper : Equatable, CustomStringConvertible {
+  let term : Term
+
+  var description: String {
+      return "\(self.term)"
+  }
+
+  static func ==(lhs: Wrapper, rhs: Wrapper) -> Bool {
+    return (lhs.term).equals (rhs.term)
+  }
+
+}
+
+func resultsOf (goal: Goal, variables: [Variable]) -> [[Variable: Wrapper]] {
+    var result = [[Variable: Wrapper]] ()
+    for s in solve (goal) {
+        let solution  = s.reified ()
+        var subresult = [Variable: Wrapper] ()
+        for v in variables {
+            subresult [v] = Wrapper (term: solution [v])
+        }
+        if !result.contains (where: { x in x == subresult }) {
+            result.append (subresult)
+        }
+    }
+    return result
+}
+
+
+// Tests
+
+class heidiTests: XCTestCase {
+
+   // Display for readability
+   func startTests() {
+      print("\n[Starting tests]\n")
+   }
+
+//==============================================================================
+// Syntax tests
+//==============================================================================
+
+   // Test heidi sequences
+   func testHeidiSeq() {
+      let res = toHeidi([deponer,davent,plaun,deponer,dretg])
+      let expected = List.cons(deponer,List.cons(davent,List.cons(plaun,List.cons(deponer,List.cons(dretg,List.empty)))))
+      XCTAssert(res.equals(expected),"Wrong sequence.")
+   }
+
+   // Test tita sequence
+   func testTitaSeq() {
+      let res = toTita([whee,whee,who,pause,wheet,short,long,pause])
+      let expected = List.cons(whee,List.cons(whee,List.cons(who,List.cons(pause,List.cons(wheet,List.cons(short,List.cons(long,List.cons(pause,List.empty))))))))
+      XCTAssert(res.equals(expected),"Wrong sequence.")
+   }
+
+//==============================================================================
+// Semantic 1 tests
+//==============================================================================
+
+   // Test Heidi -> Tita
+   func testHeidi2Tita1() {
+      let A = Variable(named: "A")
+
+      var from = toHeidi([davent,deponer,returnar])
+      var to = toTita([wheet,wheeo,wheet,wheet,pause,short,short,pause,whee,whee,wheet,pause])
+      var prob = heidi2tita1(from,A)
+      var results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toHeidi([plaun,plaun])
+      to = toTita([hee,hee,hee,hee,pause,hee,hee,hee,hee,pause])
+      prob = heidi2tita1(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toHeidi([sa_fermar,returnar,plaun,davos,davent,sanester,dretg,deponer])
+      to = toTita(
+         [
+         long,pause,
+         whee,whee,wheet,pause,
+         hee,hee,hee,hee,pause,
+         who,hee,who,pause,
+         wheet,wheeo,wheet,wheet,pause,
+         wheet,wheeo,pause,
+         whee,who,pause,
+         short,short,pause
+         ]
+      )
+      prob = heidi2tita1(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+   }
+
+   // Test Tita -> Heidi
+   func testTita2Heidi1() {
+      let A = Variable(named: "A")
+
+      var from = toTita([wheet,wheeo,wheet,wheet,pause,short,short,pause,whee,whee,wheet,pause])
+      var to = toHeidi([davent,deponer,returnar])
+      var prob = tita2heidi1(from,A)
+      var results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toTita([hee,hee,hee,hee,pause,hee,hee,hee,hee,pause])
+      to = toHeidi([plaun,plaun])
+      prob = tita2heidi1(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toTita(
+         [
+            long,pause,
+            whee,whee,wheet,pause,
+            hee,hee,hee,hee,pause,
+            who,hee,who,pause,
+            wheet,wheeo,wheet,wheet,pause,
+            wheet,wheeo,pause,
+            whee,who,pause,
+            short,short,pause
+         ]
+      )
+      to = toHeidi([sa_fermar,returnar,plaun,davos,davent,sanester,dretg,deponer])
+      prob = tita2heidi1(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+   }
+
+//==============================================================================
+// Semantic 2 tests
+//==============================================================================
+
+   // Test Heidi -> Tita
+   func testHeidi2Tita2() {
+      let A = Variable(named: "A")
+
+      var from = toHeidi([davent,deponer,returnar])
+      var to = toTita([wheet,hee,wheet,pause,wheeo,hee,wheet,pause,wheeo,wheet,pause])
+      var prob = heidi2tita2(from,A)
+      var results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toHeidi([plaun,plaun])
+      to = toTita([wheet,wheeo,wheeo,pause,wheet,wheeo,wheeo,pause])
+      prob = heidi2tita2(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toHeidi([sa_fermar,returnar,plaun,davos,davent,sanester,dretg,deponer])
+      to = toTita(
+         [
+         wheeo,wheeo,pause,
+         wheeo,wheet,pause,
+         wheet,wheeo,wheeo,pause,
+         wheet,wheeo,wheet,pause,
+         wheet,hee,wheet,pause,
+         wheet,wheeo,pause,
+         hee,wheet,pause,
+         wheeo,hee,wheet,pause
+         ]
+      )
+      prob = heidi2tita2(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+   }
+
+   // Test Tita -> Heidi
+   func testTita2Heidi2() {
+      let A = Variable(named: "A")
+
+      var from = toTita([wheet,hee,wheet,pause,wheeo,hee,wheet,pause,wheeo,wheet,pause])
+      var to = toHeidi([davent,deponer,returnar])
+      var prob = tita2heidi2(from,A)
+      var results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toTita([wheet,wheeo,wheeo,pause,wheet,wheeo,wheeo,pause])
+      to = toHeidi([plaun,plaun])
+      prob = tita2heidi2(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+
+      from = toTita(
+         [
+         wheeo,wheeo,pause,
+         wheeo,wheet,pause,
+         wheet,wheeo,wheeo,pause,
+         wheet,wheeo,wheet,pause,
+         wheet,hee,wheet,pause,
+         wheet,wheeo,pause,
+         hee,wheet,pause,
+         wheeo,hee,wheet,pause
+         ]
+      )
+      to = toHeidi([sa_fermar,returnar,plaun,davos,davent,sanester,dretg,deponer])
+      prob = tita2heidi2(from,A)
+      results = resultsOf(goal: prob, variables: [A])
+      XCTAssertEqual(results.count, 1, "Number of results is wrong")
+      if results.count == 1 {
+         XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(results)[0][A]!, Wrapper(term: to), "Result is false")
+      }
+   }
+
+   // Proof required that Heidi -> Tita -> Heidi gives back the original order
+   func proof() {
+
+      var order: Term
+      var res: [[Variable: Wrapper]]
+      let I = Variable(named: "I")
+      let R = Variable(named: "R")
+
+      // For all 8 romansh orders
+      for ord in [deponer,dretg,sanester,davent,davos,plaun,returnar,sa_fermar] {
+
+         order = List.cons(ord,List.empty)
+
+         // Find the "whistle equivalent"
+         for sol in solve(heidi2tita2(order,I)) {
+
+            // Go back to romansh
+            res = resultsOf(goal: tita2heidi2(sol[I], R), variables: [R])
+
+            // Then check if it is back to the original
+            XCTAssertEqual(res.count, 1, "Number of results is wrong")
+            if res.count == 1 {
+               XCTAssertEqual(Array<Dictionary<Variable, Wrapper>>(res)[0][R]!, Wrapper(term: order), "Result is false")
+            }
+         }
+      }
+   }
+
+
+//==============================================================================
+// Semantic 3 tests
+//==============================================================================
+
+   // Display for readability
+   func endTests() {
+      print("\n[End of tests]\n")
+   }
+
+   static var allTests : [(String, (heidiTests) -> () throws -> Void)] {
+      return [
+         ("startTests", startTests),
+         ("testHeidiSeq", testHeidiSeq),
+         ("testTitaSeq", testTitaSeq),
+         ("testHeidi2Tita1", testHeidi2Tita1),
+         ("testTita2Heidi1", testTita2Heidi1),
+         ("testHeidi2Tita2", testHeidi2Tita2),
+         ("testTita2Heidi2", testTita2Heidi2),
+         ("proof", proof),
+         ("endTests", endTests),
+      ]
+   }
+
+}
